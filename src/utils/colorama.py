b6af5635ba9a73082195bd1b691ea22e5472a434
Oleksandr <ar9241139@gmail.com>
2024-05-23 06:31:00 +0200

    import Enum

--- src/utils/colorama.py
+++ src/utils/colorama.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from  colorama import Fore, Back , Style
 
 class EnumColoramaText(Enum):
@@ -13,3 +14,9 @@ def coloramaText(text: str, colorama: EnumColoramaText) -> str:
         return text
     if colorama == EnumColoramaText.ERROR:
         return Fore.RED + f"ssd {text}"
+    
+
+# print(Fore.RED + 'Це червоний текст')
+# print(Back.GREEN + 'Це текст на зеленому фоні')
+# print(Style.RESET_ALL)
+# print('Це звичайний текст після скидання стилю')
