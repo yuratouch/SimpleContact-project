88b2d1750d540bad168d611708d6aeb7ff12df9e
Ivan Boichuk <frontenda.ua@gmail.com>
2024-05-24 12:56:56 +0300

    I need help

--- src/handlers/error_handler.py
+++ src/handlers/error_handler.py
@@ -1,7 +1,10 @@
+from functools import wraps
 # TODO: Improvement. Improve error decorator. 
 
 def input_error(func):
+    @wraps(func)
     def inner(*args, **kwargs):
+        print(func.__name__)
         try:
             return func(*args, **kwargs)
         except ValueError:
